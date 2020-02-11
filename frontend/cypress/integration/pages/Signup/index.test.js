4763d2564df344c99e49ca000b57263806f5ef70
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-11 21:19:35 +0100

    :sparkles: Create Signup page

--- frontend/cypress/integration/pages/Signup/index.test.js
+++ frontend/cypress/integration/pages/Signup/index.test.js
@@ -3,14 +3,39 @@ describe('Page Signup', function() {
     cy.visit('/auth/signup');
   });
 
-  it('Verify the compoenents: TitleHack, tree LabelHack, tree InputHack, CheckHack and ButtonHack', function() {
+  it('Verify the compoenents: TitleHack, three LabelHack, tree InputHack, CheckHack and ButtonHack', function() {
     cy.visit('/auth/signup');
     cy.get('h1').contains('Cadastro');
 
-    cy.get('label').contains('Nome');
+    cy.get('label')
+      .contains('Nome:')
+      .should('have.attr', 'for', 'name');
     cy.get('input[name=name]').should('have.attr', 'type', 'text');
+    cy.get('input[name=name]').should('have.attr', 'id', 'name');
 
-    cy.get('label').contains('Email');
-    cy.get('input[name=email]').should('have.attr', 'type', 'email');
+    cy.get('label')
+      .contains('Email:')
+      .should('have.attr', 'for', 'email');
+    cy.get('input[name=email]')
+      .should('have.attr', 'type', 'email')
+      .should('have.attr', 'id', 'email');
+
+    cy.get('label')
+      .contains('Senha:')
+      .should('have.attr', 'for', 'password');
+    cy.get('input[name=password]')
+      .should('have.attr', 'type', 'password')
+      .should('have.attr', 'id', 'password');
+
+    cy.get('input[type=checkbox]').should('have.attr', 'id', 'contract');
+    cy.get('label')
+      .contains('Li e aceito os termos de uso')
+      .should('have.attr', 'for', 'contract');
+
+    cy.contains('Li e aceito os termos de uso').click();
+    cy.get('input[type=checkbox]:checked');
+
+    cy.contains('Enviar').click();
+    cy.contains('Sua jornada de investimentos');
   });
 });
--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
 import MainOpen from '../../containers/MainOpen';
+import FormLogin from '../../containers/FormSignup';
 import TitleHack from '../../components/TitleHack';
 import LabelHack from '../../components/LabelHack';
 import InputHack from '../../components/InputHack';
+import ButtonHack from '../../components/ButtonHack';
 
 function Signup() {
   return (
     <MainOpen>
-      <TitleHack>Cadastro</TitleHack>
+      <FormLogin>
+        <TitleHack>Cadastro</TitleHack>
 
-      <LabelHack>Nome</LabelHack>
-      <InputHack name="name" type="text" />
+        <LabelHack for="name">Nome:</LabelHack>
+        <InputHack id="name" name="name" type="text" />
+
+        <LabelHack for="email">Email:</LabelHack>
+        <InputHack id="email" name="email" type="email" />
+
+        <LabelHack for="password">Senha:</LabelHack>
+        <InputHack id="password" name="password" type="password" />
+
+        <LabelHack for="contract">
+          <InputHack id="contract" type="checkbox" /> Li e aceito os termos de
+          uso
+        </LabelHack>
+
+        <ButtonHack to="/tree">Enviar</ButtonHack>
+      </FormLogin>
     </MainOpen>
   );
 }
