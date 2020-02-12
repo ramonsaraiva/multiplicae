1c66561c5f0c9740b553ff6d117990f5b8bfba2d
Ramon Saraiva <ramonsaraiva@gmail.com>
2020-02-12 09:41:34 -0300

    :whale: Fix healthcheck url

--- backend/rootfs/healthcheck.sh
+++ backend/rootfs/healthcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 # we are healthy if status.html has a good status code OR dramatiq is up
-curl --fail -L http://localhost:8000/status.html || pgrep dramatiq
+curl --fail -L http://localhost:8000/healthcheck/ || pgrep dramatiq
