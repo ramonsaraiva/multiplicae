a5e9b4e14999d8daa39fe664955afeb9e467db64
Ramon Saraiva <ramonsaraiva@gmail.com>
2020-02-12 13:33:53 -0300

    :sparkles: Add user node progress admin view

--- backend/app/analytics/admin.py
+++ backend/app/analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserNodeProgress
+
+
+@admin.register(UserNodeProgress)
+class UserNodeProgressAdmin(admin.ModelAdmin):
+    pass
