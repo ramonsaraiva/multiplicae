7dccc4bc9a24c660affcee5ec15e172461039f3b
Ramon Saraiva <ramonsaraiva@gmail.com>
2020-02-12 13:31:06 -0300

    :sparkles: Add initial user node progress model

--- backend/app/analytics/models.py
+++ backend/app/analytics/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from app.trails.models import Node
+
+
+User = get_user_model()
+
+
+class UserNodeProgress(models.Model):
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_progress')
+    node = models.ForeignKey(
+        Node, on_delete=models.CASCADE, related_name='node_progress')
+    progress = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f'{self.user} -> {self.node}: {self.progress}'
