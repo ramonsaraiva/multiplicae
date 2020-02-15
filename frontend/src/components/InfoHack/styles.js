518af8685cbefc78dcebbeed3b399b7d4c628ac7
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-15 18:04:54 +0100

    :zap: Finish InfoHack component

--- frontend/src/components/InfoHack/styles.js
+++ frontend/src/components/InfoHack/styles.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
+import { _body1 } from '../../styles/tools/Typography';
 
 export const Icon = styled.img``;
 
-export const Action = styled.a``;
+export const Action = styled.a`
+  margin-left: var(--gap-smaller);
+`;
 
-export const Info = styled.p``;
+export const Info = styled.p`
+  ${_body1};
+  display: flex;
+  align-items: center;
+  color: var(--color-primary-blue-dark);
+`;
