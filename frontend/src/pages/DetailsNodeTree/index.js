c0398a1a4f41775c35588b9e303e02515f6cae84
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-15 17:01:04 +0100

    :construction: Add the TextHack in DetailsNodeTree page

--- frontend/src/pages/DetailsNodeTree/index.js
+++ frontend/src/pages/DetailsNodeTree/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import TitleHack from '../../components/TitleHack';
 import HeaderHack from '../../containers/HeaderHack';
+import TextHack from '../../components/TextHack';
 
 function DetailsNodeTree() {
   return (
     <>
       <HeaderHack />
       <TitleHack>Lorem Ipsum</TitleHack>
+
+      <TextHack>
+        Somehow manage to catch a bird but have no idea what to do next, so play
+        with it until it dies of shock inspect anything brought into the house,
+        but scamper so i am the best hit you unexpectedly.
+      </TextHack>
     </>
   );
 }
