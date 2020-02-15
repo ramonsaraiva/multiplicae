f5790dbeb10564feef0b8af7a94d991e814f4358
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-15 18:48:55 +0100

    :sparkles: Finish InfoContent with styles

--- frontend/src/components/InfoContent/styles.js
+++ frontend/src/components/InfoContent/styles.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
+import { _caption, _h1 } from '../../styles/tools/Typography';
 
-export const Icon = styled.img``;
+export const Icon = styled.img`
+  margin-right: var(--gap-smaller);
+`;
 
-export const Strong = styled.strong``;
+export const Strong = styled.strong`
+  ${_h1};
+  color: var(--color-complementary-blue);
+  margin-right: calc(var(--gap-smaller) / 2);
+`;
 
-export const Content = styled.p``;
+export const Content = styled.p`
+  ${_caption};
+  display: flex;
+  align-items: center;
+  color: var(--color-primary-blue-dark);
+`;
