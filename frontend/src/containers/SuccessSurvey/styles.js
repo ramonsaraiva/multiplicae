145f32fa2da169a6b5d80e4f6e781342c93ef5d6
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-18 16:13:25 +0100

    :lipstick: Finish styles in SuccessSurvey component

--- frontend/src/containers/SuccessSurvey/styles.js
+++ frontend/src/containers/SuccessSurvey/styles.js
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
+import { Title } from '../../components/TitleHack/styles';
+import { Button } from '../../components/ButtonHack/styles';
 
-export const Success = styled.main``;
+export const Success = styled.main`
+  padding: var(--gap-bigger) var(--gap-big) 0;
+  text-align: center;
+
+  & > ${Title} {
+    margin-bottom: var(--gap-medium);
+  }
+  & > ${Button} {
+    height: 50px;
+    width: 100%;
+  }
+`;
