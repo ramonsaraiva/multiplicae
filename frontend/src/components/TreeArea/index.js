976ea3266532660dea5e53364922279c2780ce6f
Filipe Câncio <filipe.cancio@outlook.com>
2020-02-18 00:24:12 -0300

    🚧 changing object properts on tree page

--- frontend/src/components/TreeArea/index.js
+++ frontend/src/components/TreeArea/index.js
@@ -5,25 +5,19 @@ import { PrimaryArea, SecondaryArea } from './styles';
 function TreeArea(props) {
   return (
     <>
-      {props.node.children &&
-        props.node.children.map(e => {
-          return (
-            <>
-              <PrimaryArea>
-                <DotHack
-                  to="/details/start-tree"
-                  progress={e.progress}
-                  color="blue"
-                  dashed={!e.is_path}
-                  title={e.abbreviation}
-                />
-              </PrimaryArea>
-              <SecondaryArea>
-                <TreeArea node={e} />
-              </SecondaryArea>
-            </>
-          );
-        })}
+      <PrimaryArea>
+        <DotHack
+          to="/details/start-tree"
+          progress={props.node.progress}
+          color="blue"
+          dashed={props.node.is_path}
+          title={props.node.abbreviation}
+        />
+        <SecondaryArea>
+          {props.node.children &&
+            props.node.children.map(e => <TreeArea node={e} />)}
+        </SecondaryArea>
+      </PrimaryArea>
     </>
   );
 }
--- frontend/src/components/TreeArea/styles.js
+++ frontend/src/components/TreeArea/styles.js
@@ -1,31 +1,17 @@
 import styled from 'styled-components';
 
-export const Content = styled.div`
-  width: 100%;
+export const PrimaryArea = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  & > * {
-    width: 100%;
-    margin: 20px 10px;
-  }
-`;
-export const PrimaryArea = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  $::before {
-    content: '«';
-    color: blue;
-  }
 `;
 export const SecondaryArea = styled.div`
   display: flex;
   flex-wrap: wrap;
+  flex-direction: row;
   justify-content: space-around;
+  width: 100%;
   align-items: center;
 
   @media (max-width: 768px) {
