e4570ebaddd9dbe27019ce4cd454e6cf6f36ce9b
Marco Bruno <marco.bruno.br@gmail.com>
2020-02-19 03:03:50 +0100

    :construction: Add id to Tree

--- frontend/src/components/TreeArea/index.js
+++ frontend/src/components/TreeArea/index.js
@@ -3,13 +3,13 @@ import DotHack from '../DotHack';
 import { PrimaryArea, SecondaryArea } from './styles';
 
 function TreeArea({ node }) {
-  const { progress, color, is_path, abbreviation, children } = node;
+  const { id, progress, color, is_path, abbreviation, children } = node;
 
   return (
     <>
       <PrimaryArea>
         <DotHack
-          to="/details/start-tree"
+          to={`/node/${id}`}
           progress={progress}
           color={color}
           dashed={!is_path}
